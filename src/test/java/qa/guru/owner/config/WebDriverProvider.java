71be518122a248b3d5dc9f72ff54912021cd27a0
annashelyakina <anna.shelyakina82@gmail.com>
2025-12-21 22:40:20 +0300

    updated WebDriverProvider

--- src/test/java/qa/guru/owner/config/WebDriverProvider.java
+++ src/test/java/qa/guru/owner/config/WebDriverProvider.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.util.function.Supplier;
 
@@ -23,56 +23,53 @@ public class WebDriverProvider implements Supplier<WebDriver> {
 
     @Override
     public WebDriver get() {
-        try {
-            WebDriver driver = createDriver();
-            driver.get(config.getBaseURL());
-            return driver;
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Failed to initialize the web driver.", e); // Перебрасываем как runtime exception
-        }
+        WebDriver driver = createDriver();
+        driver.get(config.getBaseURL());
+        return driver;
     }
 
-    public WebDriver createDriver() throws MalformedURLException{
-        String remoteUrlStr = config.getRemoteURLAsString(); // Получаем URL как строку
+    public WebDriver createDriver() {
 
-        if (!remoteUrlStr.isEmpty()) { // Проверяем наличие удалённого URL
-            URL remoteUrl = new URL(remoteUrlStr); // Преобразовываем строку в URL здесь
-
-            // Создаем объект настроек в зависимости от выбранного браузера
+        if (config.getRemoteURL().isEmpty()) {
+            // Локальный запуск теста
             switch (config.getBrowser()) {
-                case CHROME:
-                    ChromeOptions chromeOptions = new ChromeOptions(); // Объект для опций Chrome
-                    chromeOptions.setCapability("browserName", config.getBrowser()); // Имя браузера
-                    chromeOptions.setCapability("browserVersion", config.getBrowserVersion()); // Версия браузера
-
-                    // Используем RemoteWebDriver для подключения к удаленному серверу
-                    return new RemoteWebDriver(new URL(remoteUrl.toString()), chromeOptions);
-                case FIREFOX:
-                    FirefoxOptions firefoxOptions = new FirefoxOptions(); // Объект для опций Firefox
-                    firefoxOptions.setCapability("browserName", config.getBrowser()); // Имя браузера
-                    firefoxOptions.setCapability("browserVersion", config.getBrowserVersion()); // Версия браузера
-
-                    // Используем RemoteWebDriver для подключения к удаленному серверу
-                    return new RemoteWebDriver(new URL(remoteUrl.toString()), firefoxOptions);
-                default:
-                    throw new IllegalArgumentException("Unsupported browser type.");
-            }
-        } else {
-            // Локальное создание драйвера
-            switch (config.getBrowser()) {
-                case CHROME:
+                case CHROME: {
                     WebDriverManager.chromedriver()
                             .browserVersion(config.getBrowserVersion())
                             .setup();
                     return new ChromeDriver();
-                case FIREFOX:
+                }
+                case FIREFOX: {
                     WebDriverManager.firefoxdriver()
                             .browserVersion(config.getBrowserVersion())
                             .setup();
                     return new FirefoxDriver();
-                default:
-                    throw new RuntimeException("Unsupported browser type.");
+                }
+                default: {
+                    throw new RuntimeException("No such driver");
+                }
             }
         }
+        else {
+            // удалённый запуск теста
+            switch (config.getBrowser()) {
+                case CHROME: {
+                    WebDriverManager.chromedriver()
+                            .browserVersion(config.getBrowserVersion())
+                            .setup();
+                    return new RemoteWebDriver(new URL(config.getRemoteURL()), new ChromeOptions());
+                }
+                case FIREFOX: {
+                    WebDriverManager.firefoxdriver()
+                            .browserVersion(config.getBrowserVersion())
+                            .setup();
+                    return new RemoteWebDriver(new URL(config.getRemoteURL()), new FirefoxOptions());
+                }
+                default: {
+                    throw new RuntimeException("No such driver");
+                }
+            }
+        }
+        throw new RuntimeException("No such browser");
     }
 }
